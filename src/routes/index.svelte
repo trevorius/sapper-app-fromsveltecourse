a26210e13eb7a28f12d45ccd94f8cd211397370a
trevo <trevorius45@gmail.com>
2022-12-04 15:47:12 -0500

    imported expense calculator

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,14 +37,7 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Expense calculator</title>
 </svelte:head>
 
-<h1>Great success!</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
